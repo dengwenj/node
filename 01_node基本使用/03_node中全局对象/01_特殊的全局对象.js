4f825442f0f1c05d71a13b10b6b07216dc71b518
dengwenj <927561237@qq.com>
2022-03-01 20:32:01 +0800

    特殊的全局对象

---
+++ 01_node基本使用/03_node中全局对象/01_特殊的全局对象.js
@@ -0,0 +1,4 @@
+console.log(__dirname); // /Users/dengwenjie/FE/myCode/node/01_node基本使用/03_node中全局对象
+console.log(__filename); // /Users/dengwenjie/FE/myCode/node/01_node基本使用/03_node中全局对象/01_特殊的全局对象.js
+// exports  module  require()
+// 这些特殊的全局对象实际上是模块的变量，只是每个模块都有
