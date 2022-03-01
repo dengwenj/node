02b4932ced785be1fef3ff12f1f74fb5cbfdf514
dengwenj <927561237@qq.com>
2022-03-01 20:52:47 +0800

    定时器函数全局对象

---
+++ 01_node基本使用/03_node中全局对象/02_定时器函数全局对象.js
@@ -0,0 +1,20 @@
+setTimeout(() => {
+  console.log('setTimeout');
+}, 1000)
+
+setInterval(() => {
+  console.log('setInterval');
+}, 1000)
+
+setImmediate(() => {
+  console.log('setImmediate');
+})
+
+
+
+
+
+
+process.nextTick(() => {
+  console.log('process.nextTick');
+})
