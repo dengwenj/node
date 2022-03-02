f333c7595e0f0bbcad3718d66b5afe3ba4d7e8a3
dengwenj <927561237@qq.com>
2022-03-02 19:01:00 +0800

    数组方法 splice 和 slice 的区别

--- 01_node基本使用/04_js-module/03_ES-Module/index.js
+++ 01_node基本使用/04_js-module/03_ES-Module/index.js
@@ -13,4 +13,14 @@ console.log(fooAll.fName)
 
 setTimeout(() => {
   console.log(name) // 变成 ww 了, 这个绑定是实时的
-}, 2000);
+}, 2000); 
+
+const arr = [1, 2, 3, 4]
+// 会修改原数组
+console.log(arr.splice(0, arr.length - 1)); // [ 1, 2, 3 ]
+console.log(arr); // [ 4 ]
+
+const arr1 = ['a', 'b', 'c']
+// 不会修改原数组
+console.log(arr1.slice(0, arr1.length - 1)); // [ 'a', 'b' ]
+console.log(arr1); // [ 'a', 'b', 'c' ]
