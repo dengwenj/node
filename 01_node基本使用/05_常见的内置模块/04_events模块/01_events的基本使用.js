7e818462aad538d71c9c9f033f2bb9032b6c02a0
dengwenj <927561237@qq.com>
2022-03-03 15:37:32 +0800

    events的基本使用

---
+++ 01_node基本使用/05_常见的内置模块/04_events模块/01_events的基本使用.js
@@ -0,0 +1,22 @@
+const EventEmitter = require('events')
+
+// 创建发射器
+const emitter = new EventEmitter()
+
+// 监听一个事件
+emitter.on('dwj', (...args) => {
+  console.log('d', args);
+})
+
+const listener2 = (...args) => {
+  console.log('w', args);
+}
+emitter.on('dwj', listener2)
+
+setTimeout(() => {
+  // 发出一个事件
+  emitter.emit('dwj', 'ww', 'jj')
+  // 关闭一个事件
+  emitter.off('dwj', listener2)
+  emitter.emit('dwj', 'ww', 'jj')
+}, 2000);
