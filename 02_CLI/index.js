1c4c5c335e3efce4da6700eff2ea8b95d50dbd0c
dengwenj <927561237@qq.com>
2022-03-03 22:45:49 +0800

    增加自己的 options 定义选项和监听命令

--- 02_CLI/index.js
+++ 02_CLI/index.js
@@ -17,11 +17,20 @@ const progarm = new Command()
 progarm.version(require('./package.json').version)
 progarm.version(require('./package.json').version, '-v --version')
 
-// 增加自己的 options
-progarm.option('-j --dwj', 'a dwj is cli')
-progarm.option('-d --dest [type]', 'a destination folder, src/components')
+// 增加自己的 options 定义选项, 这个 options 写了，在 --help 里面看的见
+progarm.option('-j --dwj', 'a dwj is cli') // dwj -j
+progarm.option('-d --dest [type]', 'a destination folder, src/components') // dwj -d src/components
+
+// 监听命令
+progarm.on('option:dest', () => {
+  console.log('-d')
+})
+progarm.on('--help', () => {
+  console.log('help')
+})
 
 // 解析
 progarm.parse(process.argv)
 
+console.log(progarm.opts()); // { dest: 'src/components' }
 console.log(progarm.opts().dest); // 这里拿到的就是你命令行输入的参数 dwj -d 后面的东西
