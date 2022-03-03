7d6bee0e9175f72cec32c835d63a3653b425f7fc
dengwenj <927561237@qq.com>
2022-03-03 22:29:21 +0800

    添加自己的options

--- 02_CLI/index.js
+++ 02_CLI/index.js
@@ -9,11 +9,19 @@
  */
 // console.log('hello cli');
 
-const progarm = require('commander')
+const { Command } = require('commander')
+
+const progarm = new Command()
 
 // 查看版本号
 progarm.version(require('./package.json').version)
-progarm.version(require('./package.json').version, '-v, --version')
+progarm.version(require('./package.json').version, '-v --version')
 
+// 增加自己的 options
+progarm.option('-j --dwj', 'a dwj is cli')
+progarm.option('-d --dest [type]', 'a destination folder, src/components')
+
+// 解析
 progarm.parse(process.argv)
- 
+
+console.log(progarm.opts().dest); // 这里拿到的就是你命令行输入的参数 dwj -d 后面的东西
