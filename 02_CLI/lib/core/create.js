b68f48d028a18dd62c14318bd5271e8ff5e0bdd1
dengwenj <927561237@qq.com>
2022-03-04 13:51:46 +0800

    删除node_modules

--- 02_CLI/lib/core/create.js
+++ 02_CLI/lib/core/create.js
@@ -5,7 +5,7 @@ const { createProjectAction } = require('./actions')
 const createCommands = () => {
   // dwj create demo
   program
-    .command('create <project> [other...]')
+    .command('create <project> [other...]') // <> 必填 []可填  ...变长参数，是个数组
     .description('创建项目')
     .action(createProjectAction)
 }
