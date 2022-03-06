68c01a45cd60fb6e67ad66ec9217ffa0a9752130
dengwenj <927561237@qq.com>
2022-03-06 14:51:34 +0800

    用 alloc 创建 buffer

---
+++ 03_node进阶/01_buffer的使用/03_buffer的alloc创建方式.js
@@ -0,0 +1,6 @@
+// 用 alloc 创建 buffer
+const buffer = Buffer.alloc(8)
+console.log(buffer) // <Buffer 00 00 00 00 00 00 00 00>
+
+buffer[0] = 0x11 // 在 js 里面用 0x 开头的直接表示的就是16进制 
+console.log(buffer) // <Buffer 11 00 00 00 00 00 00 00> 这里显示的时候是 16 进制的形式
