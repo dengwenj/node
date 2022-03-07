6d0b836238b7b755fce9d28fe532ef78b30be0b8
dengwenj <927561237@qq.com>
2022-03-07 17:00:09 +0800

    writeable的使用

---
+++ 03_node进阶/03_Stream/02_writeable的使用.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+
+const writeStream = fs.createWriteStream('./bar.txt', {
+  flags: 'a',
+  start: 3
+})
+
+// 写入
+writeStream.write('你好啊', (err) => {
+  console.log('写入成功')
+})
+
+writeStream.write('嘻嘻', (err) => {
+  console.log('写入成功')
+})
+
+writeStream.on('open', (err) => {
+  console.log('文件打开')
+})
+
+// writeStream.close()
+writeStream.end('文件关闭')
+
+writeStream.on('finish', () => {
+  console.log('文件写完');
+})
+
+writeStream.on('close', () => {
+  console.log('文件关闭');
+})
+
+
