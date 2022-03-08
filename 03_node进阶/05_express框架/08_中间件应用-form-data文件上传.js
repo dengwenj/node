67efbaffaa4361f017418d76afc035ecc4a6173c
dengwenj <927561237@qq.com>
2022-03-08 21:28:56 +0800

    文件上传

---
+++ 03_node进阶/05_express框架/08_中间件应用-form-data文件上传.js
@@ -0,0 +1,36 @@
+const path = require('path')
+
+const express = require('express')
+const multer = require('multer')
+
+const app = express()
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './upload/')
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname))
+  }
+})
+
+const upload = multer({
+  // dest: './upload/'
+  storage
+})
+
+// form-data 解析
+app.post('/login', upload.any(), (req, res) => {
+  console.log(req.body) // { username: 'dwj', password: '123' }
+  res.end('login')
+})
+
+// 文件上传
+// 传多张 key 一样 upload.array('file')，key 不一样 upload.fields([{ name: 'file', maxCount: 1 }, { name: 'hh', maxCount: 1 }])
+app.post('/upload', upload.fields([{ name: 'file', maxCount: 1 }, { name: 'hh', maxCount: 1 }]), (req, res) => {
+  res.end('文件上传成功')
+})
+
+app.listen(8000, () => {
+  console.log('启动')
+})
