4c67ada36b5730875d0cae1ec1e5cc77d3c61da2
dengwenj <927561237@qq.com>
2022-03-08 18:39:38 +0800

    中间应用

---
+++ 03_node进阶/05_express框架/06_中间件应用-parse解析.js
@@ -0,0 +1,37 @@
+const express = require('express')
+
+const app = express()
+
+// 中间件应用
+// app.use((req, res, next) => {
+//   if (req.headers['content-type'] === 'application/json') {
+//     req.on('data', (data) => {
+//       const obj = JSON.parse(data.toString())
+//       req.body = obj
+//     })
+//     req.on('end', () => {
+//       next()
+//     })
+//     return
+//   }
+//   next()
+// })
+
+app.use(express.json())
+// extended true : express 依赖的 qs 库
+// extended false : 就是 node 里面的 querystring 模块
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/login', (req, res, next) => {
+  console.log(req.body, '111')
+  res.end('login')
+})
+
+app.post('/user', (req, res, next) => {
+  console.log(req.body)
+  res.end('user')
+})
+
+app.listen(8000, () => {
+  console.log('启动');
+})
