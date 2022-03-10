aaf1c7ea06970825d2e4e9d5e6cb1c2cd0c4157c
dengwenj <927561237@qq.com>
2022-03-10 22:33:46 +0800

    GROUP BY 分组

--- 04_SQL语句/DQL-聚合函数GroupBy.sql
+++ 04_SQL语句/DQL-聚合函数GroupBy.sql
@@ -20,3 +20,14 @@ SELECT COUNT(url) FROM products WHERE brand = 'vivo';
 SELECT COUNT(price) FROM products;
 -- 重复的删除
 SELECT COUNT(DISTINCT price) FROM products;
+
+# GROUP BY 的使用   分组 
+SELECT brand, AVG(price), COUNT(*), MAX(price), AVG(score) FROM products GROUP BY brand;
+
+# HAVING 的使用 HAVING 用在 GROUP BY 筛选， WHERE 用在 数据表筛选 
+SELECT brand, AVG(price) avgPrice FROM products GROUP BY brand HAVING  avgPrice > 2000;
+
+# 求评分 score 大于 7.5 的手机的平均价格 
+SELECT AVG(price) FROM products WHERE score > 7.5;
+# score平均分大于 7.5的手机，按照品牌进行分类，求平均价格  且平均价格大于 4000 的
+SELECT brand, AVG(price) avgPrice FROM products WHERE score > 7.5 GROUP BY brand HAVING avgPrice > 4000;
