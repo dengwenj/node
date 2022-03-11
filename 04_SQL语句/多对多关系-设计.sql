e8b7f8fa92ffa2cb2d12edb2d3d3fe7bc9cc2150
dengwenj <927561237@qq.com>
2022-03-11 18:16:50 +0800

    多表之间的查询

--- 04_SQL语句/多对多关系-设计.sql
+++ 04_SQL语句/多对多关系-设计.sql
@@ -40,3 +40,43 @@ INSERT INTO students_select_courses (student_id, course_id) VALUES (1, 2);
 INSERT INTO students_select_courses (student_id, course_id) VALUES (2, 1);
 INSERT INTO students_select_courses (student_id, course_id) VALUES (1, 4);
 INSERT INTO students_select_courses (student_id, course_id) VALUES (3, 3);
+
+# 查询所有有选课的学生，选择了哪些课程 
+SELECT students.id id, students.name stuName, students.age stuAge, courses.id csId, courses.`name` csName, courses.price csPrice
+	FROM students
+	LEFT JOIN students_select_courses ON students.id = students_select_courses.student_id
+	LEFT JOIN courses ON students_select_courses.course_id = courses.id
+	WHERE students_select_courses.student_id IS NOT NULL;
+
+# 查询所有学生的选课情况 
+SELECT students.id id, students.name stuName, students.age stuAge, courses.id csId, courses.`name` csName, courses.price csPrice
+	FROM students
+	LEFT JOIN students_select_courses ON students.id = students_select_courses.student_id
+	LEFT JOIN courses ON students_select_courses.course_id = courses.id;
+
+
+# 查询哪些学生是没有选课 
+SELECT students.id id, students.name stuName, students.age stuAge, courses.id csId, courses.`name` csName, courses.price csPrice
+	FROM students
+	LEFT JOIN students_select_courses ON students.id = students_select_courses.student_id
+	LEFT JOIN courses ON students_select_courses.course_id = courses.id
+	WHERE students_select_courses.student_id IS NULL;
+	
+# 查询哪些课程是没有被选择的 
+-- SELECT *
+-- 	FROM students
+-- 	RIGHT JOIN students_select_courses ON students_select_courses.student_id = students.id
+-- 	RIGHT JOIN courses ON courses.id = students_select_courses.course_id
+SELECT * 
+	FROM courses
+	LEFT JOIN students_select_courses ON courses.id = students_select_courses.course_id
+	LEFT JOIN students ON students_select_courses.student_id = students.id
+	WHERE students.id IS NULL;
+
+
+# 查看某一个写生选择了哪些课程 （dwj）
+SELECT students.id id, students.name stuName, students.age stuAge, courses.id csId, courses.`name` csName, courses.price csPrice
+	FROM students
+	LEFT JOIN students_select_courses ON students.id = students_select_courses.student_id
+	LEFT JOIN courses ON students_select_courses.course_id = courses.id
+	WHERE students.id = 1;
