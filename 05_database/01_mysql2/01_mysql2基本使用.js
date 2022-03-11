745e067713e65a1f270dfd0d264a58f04d7f23a8
dengwenj <927561237@qq.com>
2022-03-11 21:24:21 +0800

    预处理语句

--- 05_database/01_mysql2/01_mysql2基本使用.js
+++ 05_database/01_mysql2/01_mysql2基本使用.js
@@ -13,9 +13,14 @@ const  connection = mysql.createConnection({
 
 // 执行 sql 语句 
 const statement = `
-  SELECT * FROM products;
+  SELECT * FROM products WHERE price > ? && score > ?;
 `
 
-connection.query(statement, (err, result, fields) => {
-  console.log(result);
+// connection.query(statement, (err, result, fields) => {
+//   console.log(result);
+// })
+
+// 预处理语句 会缓存，吧解析、优化、转换，执行一遍 提高性能，防止 sql 注入
+connection.execute(statement, [6000, 7], (err, result) => {
+  console.log(result)
 })
